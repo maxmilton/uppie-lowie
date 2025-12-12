47423f0bfe84e064b3643e4de36d3c8118e42f3e
Max Milton <max@happyhackers.xyz>
2025-12-12 09:10:04 +0900

    feat: Refactor logic to handle UTF8 correctly

--- src/sw.ts
+++ src/sw.ts
@@ -1,15 +1,25 @@
+/// <reference lib="webworker" /
+
 function mutatePageText() {
   const treeWalker = document.createTreeWalker(document.body, NodeFilter.SHOW_TEXT);
+  const segmenter = new Intl.Segmenter();
+  const letterRegex = /\p{Letter}/u;
 
   while (treeWalker.nextNode()) {
     const node = treeWalker.currentNode;
     const text = node.nodeValue;
 
-    if (text?.trim()) {
-      // eslint-disable-next-line no-confusing-arrow
-      node.nodeValue = Array.from(text, (char) =>
-        Math.random() < 0.5 ? char.toUpperCase() : char.toLowerCase(),
-      ).join("");
+    if (text) {
+      node.nodeValue = [...segmenter.segment(text)]
+        // eslint-disable-next-line no-confusing-arrow
+        .map(({ segment: char }) =>
+          letterRegex.test(char)
+            ? Math.random() > 0.5
+              ? char.toUpperCase()
+              : char.toLowerCase()
+            : char,
+        )
+        .join("");
     }
   }
 }
