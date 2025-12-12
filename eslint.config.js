d2c78b70dfb0e69fce3b39c36f78b8b9f511f98c
Max Milton <max@happyhackers.xyz>
2025-12-12 09:07:49 +0900

    chore: Refactor tooling

---
+++ eslint.config.js
@@ -0,0 +1,30 @@
+import js from "@eslint/js";
+import mm from "@maxmilton/eslint-config";
+import { defineConfig } from "eslint/config";
+import unicorn from "eslint-plugin-unicorn";
+import ts from "typescript-eslint";
+
+export default defineConfig(
+  js.configs.recommended,
+  ts.configs.strictTypeChecked,
+  ts.configs.stylisticTypeChecked,
+  unicorn.configs.recommended,
+  mm.configs.recommended,
+  {
+    linterOptions: {
+      reportUnusedDisableDirectives: "error",
+      reportUnusedInlineConfigs: "error",
+    },
+    languageOptions: {
+      parserOptions: {
+        projectService: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+    rules: {
+      "@typescript-eslint/no-misused-spread": "warn",
+      "no-confusing-arrow": "warn",
+    },
+  },
+  { ignores: ["dist"] },
+);
